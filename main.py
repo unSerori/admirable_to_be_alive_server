ad7f71b5238e41bc246a404b3470e0c0c9cb9ce3
unSerori <pd.ai.374005475@gmail.com>
2023-09-11 15:47:27 +0900

    [update]googleへのoauth認証ができることがわかった。python-dotenv追加

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 '''
 サバ側のmain
 
-プレイヤー情報(レベル, 各いいねポイント, 所持アイテム, デイリーミッション進捗)を保持
+flaskでエンドポイント作る。
+
+プレイヤー情報(レベル, 所持アイテム、各いいねポイントの保存、デイリーミッション進捗状況)を保持
 ログイン時に同期
 googleアカウントでログイン
 googleカレンダー
-
 '''
+import os
+from flask import Flask, jsonify, request, render_template
+from dotenv import load_dotenv  # dotenvをインポート
+load_dotenv()  # .envファイルの内容を読み込見込む
+CLIENT_ID = os.environ.get("CLIENT_ID")  # .envからクライアントIDを持ってくる。
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")  # .envからクライアントシークレットを持ってくる。
 
-import sys
 
 
-print(sys.version)
+
+app = Flask('__name__')  # インスタンス生成、これによりアクセスされたURIによって処理を変更する。
+
+print(CLIENT_ID)
+print (CLIENT_SECRET)
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
